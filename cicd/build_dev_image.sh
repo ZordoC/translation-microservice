16681d1f0feba4ee492fdf4595994995190b56e2
root <pedrogodinhoc@gmail.com>
2022-07-02 18:49:32 +0000

    Added docker dev build script.

--- cicd/build_dev_image.sh
+++ cicd/build_dev_image.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 # Bash script to create docker development image
 # PARAM is the type of version update, ie: hotfix, minor, major.
+# Run script from parent directory.
 
 PARAM=$1
 
 CURRENT_VERSION=$(docker images | awk 'FNR == 2 {print}'|  awk '{print $2}')
 
-
 if [[ ${PARAM} == 'minor' ]];
 then 
-    NEW_VERSION = 
+    NEW_VERSION=$(docker images | grep 'devimage' | awk '{print $2}' |  awk -vFS=. -vOFS=. '{$NF++;print}')
+elif [[ ${PARAM} == 'major' ]];
+then
+    NEW_VERSION=$(docker images | grep 'devimage' | awk '{print $2}' |  awk -vOFS=. '{$NF++;print}')
+else
+    echo "ARGUMENT PARSING ERROR: Patch type: Major or Minor."
+    exit
+fi
 
-
-
-
-
-
-
-
-
-docker build -f docker/Dockerfile.dev -t devimage:
+echo "Building new docker image with tag version: ${NEW_VERSION}"
+docker build -f docker/Dockerfile.dev -t $NEW_VERSION .
