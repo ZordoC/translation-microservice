bd33e8967d913e87be407dcc4520133d264e7d43
root <pedrogodinhoc@gmail.com>
2022-07-03 11:32:51 +0000

    Versioneer working

--- translate/_version.py
+++ translate/_version.py
@@ -43,7 +43,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = "None"
+    cfg.tag_prefix = ""
     cfg.parentdir_prefix = "None"
     cfg.versionfile_source = "translate/_version.py"
     cfg.verbose = False
